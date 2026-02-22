7493fa996794c05b0890dcac745f4b51839d0bf3
rafiq <h.rafiq0738@uca.ac.ma>
2026-02-22 14:59:13 +0000

    Add 3 JavaScript exercises

--- exercices/ex1_variables.js
+++ exercices/ex1_variables.js
@@ -1,6 +1,6 @@
 function infosEtudiant(nom, note) {
     function resultat(note){
-       return (note >= 10) ? "Admis" : "Ajourné";  // ← return est essentiel !
+       return (note >= 10) ? "Admis" : "Ajourné";
     }
     return nom + " : " + note + "/20 → " + resultat(note);
 }
